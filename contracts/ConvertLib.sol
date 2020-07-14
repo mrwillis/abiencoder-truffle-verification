b31ecdaf02d14777758c54cec0a976cc52d26497
Julian Wilson <julian@nextgenbt.com>
2020-07-14 14:44:45 -0400

    Simpler reproduction

--- contracts/ConvertLib.sol
+++ contracts/ConvertLib.sol
@@ -1,9 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.5.16;
+pragma experimental ABIEncoderV2;
 
 
 library ConvertLib{
-	function convert(uint amount,uint conversionRate) public pure returns (uint convertedAmount)
+	function convert(uint amount,uint conversionRate) internal pure returns (uint convertedAmount)
 	{
 		return amount * conversionRate;
 	}
